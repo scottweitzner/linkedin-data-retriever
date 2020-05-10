44ad9c1b3e77a234d5ad9501c9064916c334dd79
Scott Weitzner <scottweitzner@gmail.com>
2020-05-09 22:55:44 -0400

    finished scraping experience

--- scrape_linkedin.py
+++ scrape_linkedin.py
@@ -1,43 +1,70 @@
+import json
+
 from bs4 import BeautifulSoup
-import pprint as pp
 
 data = {}
 
 
 def scrape_experience(soup):
 
-    def scrape_position_info_single(experience_card):
-        # print(experience_card)
-        company_info = experience_card.select_one('.pv-entity__summary-info')
+    def get_paragraph_text(element):
+        contents = element.decode_contents().split('\n')[0]
+        contents = contents.replace('<br/>', '\n')
+        return contents
+
+    def get_position_details(position_card):
+        title = position_card.select_one('h3')
+        title = title.select_one('span:nth-of-type(2)') if title.select('span') else title
+        location = position_card.select_one('.pv-entity__location > span:nth-of-type(2)')
+        attachment = position_card.select_one('figure')
+        attachment_info = None
+        if attachment:
+            attachment_info = {
+                'title': attachment.select_one('h5').text.strip(),
+                'link': 'https://linkedin.com' + position_card.find('a', attrs={'data-control-name': 'treasury_thumbnail_cell'})['href']
+            }
+
         return {
-            'company': company_info.select_one('.pv-entity__secondary-title').text.strip(),
-            'positions': [
-                {
-                    'title': company_info.select_one('h3').text.strip(),
-                    'details': experience_card.select_one('.pv-entity__extra-details > p').text
-                }
-            ]
+            'title': title.text.strip(),
+            'location': location.text.strip() if location else None,
+            'details': get_paragraph_text(position_card.select_one('.pv-entity__extra-details > p')),
+            'duration': position_card.select_one('.pv-entity__bullet-item-v2').text.strip(),
+            'dates': position_card.select_one('.pv-entity__date-range > span:nth-of-type(2)').text.strip(),
+            'attachment': attachment_info
         }
 
-    def scrape_position_info_multiple(experience_card):
-        company_info = experience_card.select_one('.pv-entity__company-summary-info')
+    def scrape_position_info_single(position_card):
         return {
-            'company': company_info.select_one('h3 > span:nth-of-type(2)').text.strip(),
-            'total_duration': company_info.select_one('h4 > span:nth-of-type(2)').text.strip()
+            'company': position_card.select_one('.pv-entity__secondary-title').text.strip(),
+            'total_duration': position_card.select_one('.pv-entity__bullet-item-v2').text.strip(),
+            'positions': [get_position_details(position_card)]
         }
 
+    def scrape_position_info_multiple(position_card):
+        top_level_company_info = position_card.select_one('.pv-entity__company-summary-info')
+        position_details = [get_position_details(position) for position in position_card.select('.pv-entity__position-group-role-item')]
+        return {
+            'company': top_level_company_info.select_one('h3 > span:nth-of-type(2)').text.strip(),
+            'total_duration': top_level_company_info.select_one('h4 > span:nth-of-type(2)').text.strip(),
+            'positions': position_details
+        }
+
+    experience = []
     experience_cards = soup.select('.pv-profile-section__card-item-v2')
     for experience_card in experience_cards:
-        if experience_card.select_one('.pv-entity__summary-info'):
-            pp.pprint(scrape_position_info_single(experience_card), indent=4)
+        if experience_card.select_one('ul'):
+            experience.append(scrape_position_info_multiple(experience_card))
         else:
-            scrape_position_info_multiple(experience_card)
+            experience.append(scrape_position_info_single(experience_card))
+    return experience
 
 
 def main():
     with open('linkedin_profile.html', 'rb') as profile_html:
         soup = BeautifulSoup(profile_html.read(), 'html.parser')
-    scrape_experience(soup)
+    data['positions'] = scrape_experience(soup)
+    with open('linkedin_profile.json', 'w') as outfile:
+        json.dump(data, outfile, indent=4)
 
 
 if __name__ == '__main__':
